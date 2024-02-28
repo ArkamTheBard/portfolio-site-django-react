e11f3a9c22321eccf090e71fca0cf8dddaf8f8db
Yardley Gutierrez <yardleyg9@gmail.com>
2024-02-28 04:44:26 -0600

    Trying to fix api endpoint connection from frontend

--- api/api/settings.py
+++ api/api/settings.py
@@ -144,5 +144,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://yardleygutierrez.com',
+    'https://www.yardleygutierrez.com',
 ]
